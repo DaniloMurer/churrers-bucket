07baaaee4869a0828a36be615b0cef45358386e8
Danilo Jakob <danilo.jakob@gmx.ch>
2023-09-23 02:58:55 +0200

    feat(client): home view is also responsive now wohooo

--- churrers-ship/churrers-visitors/client/src/views/Home.vue
+++ churrers-ship/churrers-visitors/client/src/views/Home.vue
@@ -60,11 +60,11 @@ export default {
 </script>
 
 <template>
-  <main style="width: 100%; height: 100%">
-    <div class="container-xxl" style="width: 100rem">
-      <div class="text-center" style="margin: 2rem">
+  <div class="flex-container">
+    <div>
+      <div class="text-center" style="margin-top: 2rem">
         <h1>Visitor Dashboard</h1>
-        <router-link tag="li" class="color-background nav-item"  to="/messageboard">To the Message Board</router-link>
+        <b> VERY IMPORTANT: <router-link tag="li" class="color-background nav-item"  to="/messageboard">To the Message Board</router-link></b>
         <h5 class="text-secondary" style="margin: 1.5rem"><img src="../assets/cat.png" style="height: 28px; width: 28px"/><b>Cat fact of the visit:</b> {{ this.catFact }}</h5>
       </div>
       <br>
@@ -72,9 +72,9 @@ export default {
         <div class="text-center">
           <h2 class="text-secondary">Running Containers</h2>
         </div>
-        <div class="col" v-for="item in this.dockerContainers" :key="item.Id">
-          <div class="card" style="margin: 1rem 0 1rem 0; height: 8rem">
-            <div class="card-body" style="width: 20rem; height: 4rem">
+        <div class="col col-flex-container">
+          <div v-for="item in this.dockerContainers" :key="item.Id" class="card" style="margin: 1rem 0 1rem 0; height: 8rem; width: 70%">
+            <div class="card-body" style="height: 4rem">
               <h5 class="card-title">{{item.Names[0]}}</h5>
               <h5 class="card-subtitle mb-2 text-body-secondary">{{item.State}}</h5>
               <p>{{item.Status}}</p>
@@ -83,10 +83,10 @@ export default {
         </div>
       </div>
       <div class="row">
-        <div class="col">
+        <div class="col col-flex-container">
           <h2 class="text-secondary">Last 10 Visitors</h2>
-          <div class="card" v-for="item in this.visitors" :key="item.id" style="margin: 1rem 0 1rem 0; height: 33rem">
-            <img v-bind:src="item.icon" alt="fuck" style="width: 100%; height: 100%"/>
+          <div class="card" v-for="item in this.visitors" :key="item.id" style="margin: 1rem 0 1rem 0; width: 15rem; height: 23rem">
+            <img v-bind:src="item.icon" alt="fuck" style="width: 100%; height: auto; border-radius: 0.5rem"/>
             <div class="card-body" style="width: 15rem; height: 8rem">
               <h5 class="card-title">{{item.countryIso}}</h5>
               <h5 class="card-subtitle mb-2 text-body-secondary">{{item.country}}</h5>
@@ -94,17 +94,17 @@ export default {
             </div>
           </div>
         </div>
-        <div class="col">
+        <div class="col col-flex-container">
           <h2 class="text-secondary">Countries</h2>
-          <div class="card" v-for="item in this.countries" :key="item" style="margin: 1rem 0 1rem 0">
+          <div class="card" v-for="item in this.countries" :key="item" style="margin: 1rem 0 1rem 0; width: 15rem">
             <div class="card-body" style="width: 10rem; height: 3rem">
               <h5 class="card-title">{{item}}</h5>
             </div>
           </div>
         </div>
-        <div class="col">
+        <div class="col col-flex-container">
           <h2 class="text-secondary">Cities</h2>
-          <div class="card" v-for="item in this.cities" :key="item" style="margin: 1rem 0 1rem 0">
+          <div class="card" v-for="item in this.cities" :key="item" style="margin: 1rem 0 1rem 0; width: 15rem">
             <div class="card-body" style="width: 10rem; height: 3rem">
               <h5 class="card-title">{{item}}</h5>
             </div>
@@ -112,13 +112,20 @@ export default {
         </div>
       </div>
     </div>
-  </main>
+  </div>
 </template>
 
 <style scoped>
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
+.flex-container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+}
+.col-flex-container {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
 }
 </style>
