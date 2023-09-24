299f5c8abbdf7c53d654834ea639747248a985e3
Danilo Jakob <danilo.jakob@gmx.ch>
2023-09-24 06:52:28 +0200

    feat(client): you can now select on the messageboard how many messages should be shown instead of 20 only.

--- churrers-ship/churrers-visitors/client/src/views/MessageBoard.vue
+++ churrers-ship/churrers-visitors/client/src/views/MessageBoard.vue
@@ -4,7 +4,8 @@ export default {
     return {
       messages: [],
       currentMessage: "",
-      messagesLength: ""
+      messagesLength: "",
+      messagesAmount: 20
     }
   },
   created() {
@@ -27,11 +28,14 @@ export default {
       })
     },
     fetchMessages: function() {
-      fetch('https://api.visitors.churrer.ch/messages?length=20').then(data => {
+      fetch(`https://api.visitors.churrer.ch/messages?length=${this.messagesAmount}`).then(data => {
         return data.json();
       }).then( json => {
         this.messages = json.messages;
       });
+    },
+    onMessagesAmountChange: function() {
+      this.fetchMessages();
     }
   }
 }
@@ -43,12 +47,18 @@ export default {
       <div class="text-center" style="margin: 2rem">
         <h1>Message Board - "Talk" to strangers</h1>
         <h5 class="text-secondary">Type message and press 'Enter' key</h5>
-        <h6 class="text-secondary">The last 20 messages are shown</h6>
+        <h6 class="text-secondary">Per default the last 20 messages are shown</h6>
         <h6 class="text-secondary">Use "<img style="width: 1.5rem;" src="../assets/windows.jpg"/> + . " for Emojis</h6>
       </div>
       <br>
-      <div style="width: 80%">
-        <input placeholder="Hello world!" class="form-control" v-model="this.currentMessage" type="text" @keydown.enter="this.onSubmitMessage()" style="width: 100%"/>
+      <div class="flex-container-row" style="width: 90%">
+        <input placeholder="Hello world!" class="form-control" v-model="this.currentMessage" type="text" @keydown.enter="this.onSubmitMessage()" style="width: 80%"/>
+        <select v-model="this.messagesAmount" @change="onMessagesAmountChange" class="form-select" style="width: 19.5%; margin-left: 0.5%;">
+          <option selected>20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+        </select>
       </div>
       <br>
       <ul class="list-group" style="border: 1rem; width: 90%">
@@ -63,6 +73,13 @@ export default {
   display: flex;
   /*flex-direction: column;*/
   flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+}
+.flex-container-row {
+  display: flex;
+  /*flex-direction: column;*/
+  flex-flow: row wrap;
   justify-content: center;
   align-items: center;
 }
